a2e796ffc89a5904b70ca2137993c5bbd05fe2ef
Alina Wang <alinawang.ubc@gmail.com>
2021-08-31 22:37:29 -0700

    easy problems

---
+++ leetcode/editor/en/_94_BinaryTreeInorderTraversal.java
@@ -0,0 +1,112 @@
+//Given the root of a binary tree, return the inorder traversal of its nodes' va
+//lues. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: root = [1,null,2,3]
+//Output: [1,3,2]
+// 
+//
+// Example 2: 
+//
+// 
+//Input: root = []
+//Output: []
+// 
+//
+// Example 3: 
+//
+// 
+//Input: root = [1]
+//Output: [1]
+// 
+//
+// Example 4: 
+//
+// 
+//Input: root = [1,2]
+//Output: [2,1]
+// 
+//
+// Example 5: 
+//
+// 
+//Input: root = [1,null,2]
+//Output: [1,2]
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The number of nodes in the tree is in the range [0, 100]. 
+// -100 <= Node.val <= 100 
+// 
+//
+// 
+//Follow up: Recursive solution is trivial, could you do it iteratively? Related
+// Topics Stack Tree Depth-First Search Binary Tree 
+// 👍 5550 👎 236
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+import javax.swing.tree.TreeNode;
+
+public class _94_BinaryTreeInorderTraversal {
+  private static final Logger logger = Logger.getLogger(_94_BinaryTreeInorderTraversal.class.toString());
+
+  public static void main(String[] args) {
+    long startTimeMillis = System.currentTimeMillis();
+    Solution solution = new _94_BinaryTreeInorderTraversal().new Solution();
+    // assert solution == ;
+    logger.warning(String.valueOf(solution));
+    logger.info("time cost: [" + (System.currentTimeMillis() - startTimeMillis) + "] ms");
+  }
+  //leetcode submit region begin(Prohibit modification and deletion)
+
+  /**
+   * Definition for a binary tree node. public class TreeNode { int val; TreeNode left; TreeNode
+   * right; TreeNode() {} TreeNode(int val) { this.val = val; } TreeNode(int val, TreeNode left,
+   * TreeNode right) { this.val = val; this.left = left; this.right = right; } }
+   */
+  class Solution {
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+
+      TreeNode() {
+      }
+
+      TreeNode(int val) {
+        this.val = val;
+      }
+
+      TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+      }
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+      List<Integer> result = new ArrayList<>();
+      helper(root,result);
+      return result;
+    }
+
+    private void helper(TreeNode root, List<Integer> result) {
+      if (root != null) {
+        helper(root.left, result);
+        result.add(root.val);
+        helper(root.right, result);
+      }
+    }
+  }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
