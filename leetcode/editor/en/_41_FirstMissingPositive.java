97cceeb0531b0fbcfddd36d2baafa42516eba20e
Alina Wang <alinawang.ubc@gmail.com>
2021-09-30 14:31:44 -0700

    ...

--- leetcode/editor/en/_41_FirstMissingPositive.java
+++ leetcode/editor/en/_41_FirstMissingPositive.java
@@ -25,23 +25,45 @@
 // Related Topics Array Hash Table
 // 👍 7192 👎 1131
 
+import java.util.Arrays;
 import java.util.logging.Logger;
-public class _41_FirstMissingPositive{
-    private static final Logger logger = Logger.getLogger(_41_FirstMissingPositive .class.toString());
-    public static void main(String[] args) {
-        long startTimeMillis = System.currentTimeMillis();
-        Solution solution = new _41_FirstMissingPositive().new Solution();
-        // assert solution == ;
-        logger.warning(String.valueOf(solution));
-        logger.info("time cost: [" + (System.currentTimeMillis() - startTimeMillis) + "] ms");
-    }
-    //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public int firstMissingPositive(int[] nums) {
-      
 
+public class _41_FirstMissingPositive {
+  private static final Logger logger = Logger.getLogger(_41_FirstMissingPositive.class.toString());
+
+  public static void main(String[] args) {
+    long startTimeMillis = System.currentTimeMillis();
+    Solution solution = new _41_FirstMissingPositive().new Solution();
+    int[] nums = {1};
+    solution.firstMissingPositive(nums);
+    logger.warning(String.valueOf(solution));
+    logger.info("time cost: [" + (System.currentTimeMillis() - startTimeMillis) + "] ms");
+  }
+
+  //leetcode submit region begin(Prohibit modification and deletion)
+  class Solution {
+    public int firstMissingPositive(int[] nums) {
+      int len = nums.length;
+      int result = len+1;
+      int[] positiveInt = new int[len];
+      for (int i = 0; i < len; i++) {
+        positiveInt[i] = i + 1;
+      }
+      for (int i = 0; i < len; i++) {
+        if (nums[i] > 0 && nums[i] < len + 1) {
+          positiveInt[nums[i]-1] = 0;
+        }
+      }
+      for (int i = 0; i < len; i++) {
+        if (positiveInt[i] > 0) {
+          result = positiveInt[i];
+          break;
+        }
+      }
+      System.out.print(result);
+      return result;
     }
-}
+  }
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
