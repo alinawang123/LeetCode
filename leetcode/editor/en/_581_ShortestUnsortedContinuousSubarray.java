1975b2495160134a644638aaa950bc695314e921
Alina Wang <alinawang.ubc@gmail.com>
2021-09-29 19:02:45 -0700

    ...

--- leetcode/editor/en/_581_ShortestUnsortedContinuousSubarray.java
+++ leetcode/editor/en/_581_ShortestUnsortedContinuousSubarray.java
@@ -1,62 +1,75 @@
-////Given an integer array nums, you need to find one continuous subarray that if
-////you only sort this subarray in ascending order, then the whole array will be sor
-////ted in ascending order.
-////
-//// Return the shortest such subarray and output its length.
-////
-////
-//// Example 1:
-////
-////
-////Input: nums = [2,6,4,8,10,9,15]
-////Output: 5
-////Explanation: You need to sort [6, 4, 8, 10, 9] in ascending order to make the
-////whole array sorted in ascending order.
-////
-////
-//// Example 2:
-////
-////
-////Input: nums = [1,2,3,4]
-////Output: 0
-////
-////
-//// Example 3:
-////
-////
-////Input: nums = [1]
-////Output: 0
-////
-////
-////
-//// Constraints:
-////
-////
-//// 1 <= nums.length <= 104
-//// -105 <= nums[i] <= 105
-////
-////
-////
-////Follow up: Can you solve it in O(n) time complexity? Related Topics Array Two
-////Pointers Stack Greedy Sorting Monotonic Stack
-//// 👍 4387 👎 193
+//Given an integer array nums, you need to find one continuous subarray that if
+//you only sort this subarray in ascending order, then the whole array will be sor
+//ted in ascending order.
 //
-//import java.util.logging.Logger;
-//public class _581_ShortestUnsortedContinuousSubarray{
-//    private static final Logger logger = Logger.getLogger(_581_ShortestUnsortedContinuousSubarray .class.toString());
-//    public static void main(String[] args) {
-//        long startTimeMillis = System.currentTimeMillis();
-//        Solution solution = new _581_ShortestUnsortedContinuousSubarray().new Solution();
-//        // assert solution == ;
-//        logger.warning(String.valueOf(solution));
-//        logger.info("time cost: [" + (System.currentTimeMillis() - startTimeMillis) + "] ms");
-//    }
-//    //leetcode submit region begin(Prohibit modification and deletion)
-//class Solution {
-//    public int findUnsortedSubarray(int[] nums) {
+// Return the shortest such subarray and output its length.
 //
-//    }
-//}
-////leetcode submit region end(Prohibit modification and deletion)
 //
-//}
+// Example 1:
+//
+//
+//Input: nums = [2,6,4,8,10,9,15]
+//Output: 5
+//Explanation: You need to sort [6, 4, 8, 10, 9] in ascending order to make the
+//whole array sorted in ascending order.
+//
+//
+// Example 2:
+//
+//
+//Input: nums = [1,2,3,4]
+//Output: 0
+//
+//
+// Example 3:
+//
+//
+//Input: nums = [1]
+//Output: 0
+//
+//
+//
+// Constraints:
+//
+//
+// 1 <= nums.length <= 104
+// -105 <= nums[i] <= 105
+//
+//
+//
+//Follow up: Can you solve it in O(n) time complexity? Related Topics Array Two
+//Pointers Stack Greedy Sorting Monotonic Stack
+// 👍 4387 👎 193
+
+import java.util.Stack;
+import java.util.logging.Logger;
+public class _581_ShortestUnsortedContinuousSubarray{
+    private static final Logger logger = Logger.getLogger(_581_ShortestUnsortedContinuousSubarray .class.toString());
+    public static void main(String[] args) {
+        long startTimeMillis = System.currentTimeMillis();
+        Solution solution = new _581_ShortestUnsortedContinuousSubarray().new Solution();
+        int[] input = {2,6,4,8,10,9,15};
+        solution.findUnsortedSubarray(input);
+        logger.warning(String.valueOf(solution));
+        logger.info("time cost: [" + (System.currentTimeMillis() - startTimeMillis) + "] ms");
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int findUnsortedSubarray(int[] nums) {
+      int len = nums.length - 1, left = -1, right = -1,
+          max = nums[0], min = nums[len];
+      for (int i = 1; i <= len; i++) {
+        int a = nums[i], b = nums[len-i];
+        if (a < max) right = i;
+        else max = a;
+        if (b > min) left = i;
+        else min = b;
+      }
+      return Math.max(0, left + right - len + 1);
+      }
+
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
--- leetcode/editor/en/_739_DailyTemperatures.java
+++ leetcode/editor/en/_739_DailyTemperatures.java
@@ -1,45 +1,60 @@
-////Given an array of integers temperatures represents the daily temperatures, ret
-////urn an array answer such that answer[i] is the number of days you have to wait a
-////fter the ith day to get a warmer temperature. If there is no future day for whic
-////h this is possible, keep answer[i] == 0 instead.
-////
-////
-//// Example 1:
-//// Input: temperatures = [73,74,75,71,69,72,76,73]
-////Output: [1,1,4,2,1,1,0,0]
-//// Example 2:
-//// Input: temperatures = [30,40,50,60]
-////Output: [1,1,1,0]
-//// Example 3:
-//// Input: temperatures = [30,60,90]
-////Output: [1,1,0]
-////
-////
-//// Constraints:
-////
-////
-//// 1 <= temperatures.length <= 105
-//// 30 <= temperatures[i] <= 100
-////
-//// Related Topics Array Stack Monotonic Stack
-//// 👍 5068 👎 139
+//Given an array of integers temperatures represents the daily temperatures, ret
+//urn an array answer such that answer[i] is the number of days you have to wait a
+//fter the ith day to get a warmer temperature. If there is no future day for whic
+//h this is possible, keep answer[i] == 0 instead.
 //
-//import java.util.logging.Logger;
-//public class _739_DailyTemperatures{
-//    private static final Logger logger = Logger.getLogger(_739_DailyTemperatures .class.toString());
-//    public static void main(String[] args) {
-//        long startTimeMillis = System.currentTimeMillis();
-//        Solution solution = new _739_DailyTemperatures().new Solution();
-//        // assert solution == ;
-//        logger.warning(String.valueOf(solution));
-//        logger.info("time cost: [" + (System.currentTimeMillis() - startTimeMillis) + "] ms");
-//    }
-//    //leetcode submit region begin(Prohibit modification and deletion)
-//class Solution {
-//    public int[] dailyTemperatures(int[] temperatures) {
 //
-//    }
-//}
-////leetcode submit region end(Prohibit modification and deletion)
+// Example 1:
+// Input: temperatures = [73,74,75,71,69,72,76,73]
+//Output: [1,1,4,2,1,1,0,0]
+// Example 2:
+// Input: temperatures = [30,40,50,60]
+//Output: [1,1,1,0]
+// Example 3:
+// Input: temperatures = [30,60,90]
+//Output: [1,1,0]
 //
-//}
+//
+// Constraints:
+//
+//
+// 1 <= temperatures.length <= 105
+// 30 <= temperatures[i] <= 100
+//
+// Related Topics Array Stack Monotonic Stack
+// 👍 5068 👎 139
+
+import java.util.Stack;
+import java.util.logging.Logger;
+
+public class _739_DailyTemperatures {
+  private static final Logger logger = Logger.getLogger(_739_DailyTemperatures.class.toString());
+
+  public static void main(String[] args) {
+    long startTimeMillis = System.currentTimeMillis();
+    Solution solution = new _739_DailyTemperatures().new Solution();
+    int[] input = {73, 74, 75, 71, 69, 72, 76, 73};
+    solution.dailyTemperatures(input);
+    logger.warning(String.valueOf(solution));
+    logger.info("time cost: [" + (System.currentTimeMillis() - startTimeMillis) + "] ms");
+  }
+
+  //leetcode submit region begin(Prohibit modification and deletion)
+  class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+      int len = temperatures.length;
+      Stack<Integer> stack = new Stack<>();
+      int[] output = new int[len];
+      for (int i = 0; i < len; i++) {
+        while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+          int index = stack.pop();
+          output[index] = i - index;
+        }
+        stack.push(i);
+      }
+      return output;
+    }
+  }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
