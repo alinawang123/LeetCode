2793e9f899c4947499195264201e5ed12ed72dc5
Alina Wang <alinawang.ubc@gmail.com>
2021-12-02 11:57:51 -0800

    。。。

---
+++ leetcode/editor/en/_55_JumpGame.java
@@ -0,0 +1,67 @@
+//You are given an integer array nums. You are initially positioned at the array
+//'s first index, and each element in the array represents your maximum jump lengt
+//h at that position. 
+//
+// Return true if you can reach the last index, or false otherwise. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: nums = [2,3,1,1,4]
+//Output: true
+//Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: nums = [3,2,1,0,4]
+//Output: false
+//Explanation: You will always arrive at index 3 no matter what. Its maximum jum
+//p length is 0, which makes it impossible to reach the last index.
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= nums.length <= 104 
+// 0 <= nums[i] <= 105 
+// 
+// Related Topics Array Dynamic Programming Greedy 
+// 👍 8896 👎 524
+
+import java.util.logging.Logger;
+
+public class _55_JumpGame {
+  private static final Logger logger = Logger.getLogger(_55_JumpGame.class.toString());
+
+  public static void main(String[] args) {
+    long startTimeMillis = System.currentTimeMillis();
+    Solution solution = new _55_JumpGame().new Solution();
+    // assert solution == ;
+    logger.warning(String.valueOf(solution));
+    logger.info("time cost: [" + (System.currentTimeMillis() - startTimeMillis) + "] ms");
+  }
+
+  //leetcode submit region begin(Prohibit modification and deletion)
+  class Solution {
+    public boolean canJump(int[] nums) {
+      int curEnd = 0, curFarthest = 0;
+      for (int i = 0; i < nums.length - 1; i++) {
+        curFarthest = Math.max(curFarthest, i + nums[i]);
+        if (i == curEnd) {
+          curEnd = curFarthest;
+        }
+      }
+      if (curEnd < nums.length - 1) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+  }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ leetcode/editor/en/_56_MergeIntervals.java
@@ -0,0 +1,69 @@
+//Given an array of intervals where intervals[i] = [starti, endi], merge all ove
+//rlapping intervals, and return an array of the non-overlapping intervals that co
+//ver all the intervals in the input. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+//Output: [[1,6],[8,10],[15,18]]
+//Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+// 
+//
+// Example 2: 
+//
+// 
+//Input: intervals = [[1,4],[4,5]]
+//Output: [[1,5]]
+//Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= intervals.length <= 104 
+// intervals[i].length == 2 
+// 0 <= starti <= endi <= 104 
+// 
+// Related Topics Array Sorting 
+// 👍 10556 👎 455
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.logging.Logger;
+public class _56_MergeIntervals{
+    private static final Logger logger = Logger.getLogger(_56_MergeIntervals .class.toString());
+    public static void main(String[] args) {
+        long startTimeMillis = System.currentTimeMillis();
+        Solution solution = new _56_MergeIntervals().new Solution();
+        // assert solution == ;
+        logger.warning(String.valueOf(solution));
+        logger.info("time cost: [" + (System.currentTimeMillis() - startTimeMillis) + "] ms");
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        if(intervals==null|| intervals.length<=1) return intervals;
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));// Array and arraylist not
+        // familiar
+        List<int[]> output = new ArrayList<>(); //wasn't aware of this can be done.
+        int[] temp= intervals[0];
+        output.add(temp);
+        for (int[] interval: intervals) {
+            if (interval[0] <= temp[1]) {
+                temp[1] = Math.max(temp[1], interval[1]);
+            } else {
+                temp = interval;
+                output.add(temp);
+            }
+        }
+        return output.toArray(new int[output.size()][]);
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
