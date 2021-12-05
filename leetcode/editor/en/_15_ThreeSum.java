5d7173ddaa15ca1a4906ce88a6ba2c7fb0e7c7b4
Alina Wang <alinawang.ubc@gmail.com>
2021-12-04 23:16:02 -0800

    。。。

--- leetcode/editor/en/_15_ThreeSum.java
+++ leetcode/editor/en/_15_ThreeSum.java
@@ -24,22 +24,55 @@
 // Related Topics Array Two Pointers Sorting 
 // 👍 14580 👎 1402
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Logger;
-public class _15_ThreeSum{
-    private static final Logger logger = Logger.getLogger(_15_ThreeSum .class.toString());
-    public static void main(String[] args) {
-        long startTimeMillis = System.currentTimeMillis();
-        Solution solution = new _15_ThreeSum().new Solution();
-        // assert solution == ;
-        logger.warning(String.valueOf(solution));
-        logger.info("time cost: [" + (System.currentTimeMillis() - startTimeMillis) + "] ms");
-    }
-    //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
+import java.util.stream.Collector;
+
+public class _15_ThreeSum {
+  private static final Logger logger = Logger.getLogger(_15_ThreeSum.class.toString());
+
+  public static void main(String[] args) {
+    long startTimeMillis = System.currentTimeMillis();
+    Solution solution = new _15_ThreeSum().new Solution();
+    int[] nums = new int[]{-1, 0, 1, 2, -1, -4};
+    solution.threeSum(nums);
+    logger.warning(String.valueOf(solution));
+    logger.info("time cost: [" + (System.currentTimeMillis() - startTimeMillis) + "] ms");
+  }
+
+  //leetcode submit region begin(Prohibit modification and deletion)
+  class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        
+      List<List<Integer>> res = new ArrayList<>();
+      Arrays.sort(nums);
+      for (int i = 0; i + 2 < nums.length; i++) {
+        if (i > 0 && nums[i] == nums[i - 1]) {              // skip same result
+          continue;
+        }
+        int j = i + 1, k = nums.length - 1;
+        int target = -nums[i];
+        while (j < k) {
+          if (nums[j] + nums[k] == target) {
+            res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+            j++;
+            k--;
+            while (j < k && nums[j] == nums[j - 1]) j++;  // skip same result
+            while (j < k && nums[k] == nums[k + 1]) k--;  // skip same result
+          } else if (nums[j] + nums[k] > target) {
+            k--;
+          } else {
+            j++;
+          }
+        }
+      }
+      return res;
     }
+  }
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
-}
+
---
+++ leetcode/editor/en/_371_SumOfTwoIntegers.java
@@ -0,0 +1,39 @@
+//Given two integers a and b, return the sum of the two integers without using t
+//he operators + and -. 
+//
+// 
+// Example 1: 
+// Input: a = 1, b = 2
+//Output: 3
+// Example 2: 
+// Input: a = 2, b = 3
+//Output: 5
+// 
+// 
+// Constraints: 
+//
+// 
+// -1000 <= a, b <= 1000 
+// 
+// Related Topics Math Bit Manipulation 
+// 👍 2125 👎 3260
+
+import java.util.logging.Logger;
+public class _371_SumOfTwoIntegers{
+    private static final Logger logger = Logger.getLogger(_371_SumOfTwoIntegers .class.toString());
+    public static void main(String[] args) {
+        long startTimeMillis = System.currentTimeMillis();
+        Solution solution = new _371_SumOfTwoIntegers().new Solution();
+        // assert solution == ;
+        logger.warning(String.valueOf(solution));
+        logger.info("time cost: [" + (System.currentTimeMillis() - startTimeMillis) + "] ms");
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int getSum(int a, int b) {
+        
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
