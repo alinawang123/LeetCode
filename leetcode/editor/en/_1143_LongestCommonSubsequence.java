694606aef75bd606400b39f8ec5c7ea88aacd0eb
Alina Wang <alinawang.ubc@gmail.com>
2021-12-05 21:46:40 -0800

    ...

---
+++ leetcode/editor/en/_1143_LongestCommonSubsequence.java
@@ -0,0 +1,83 @@
+//Given two strings text1 and text2, return the length of their longest common s
+//ubsequence. If there is no common subsequence, return 0. 
+//
+// A subsequence of a string is a new string generated from the original string 
+//with some characters (can be none) deleted without changing the relative order o
+//f the remaining characters. 
+//
+// 
+// For example, "ace" is a subsequence of "abcde". 
+// 
+//
+// A common subsequence of two strings is a subsequence that is common to both s
+//trings. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: text1 = "abcde", text2 = "ace" 
+//Output: 3  
+//Explanation: The longest common subsequence is "ace" and its length is 3.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: text1 = "abc", text2 = "abc"
+//Output: 3
+//Explanation: The longest common subsequence is "abc" and its length is 3.
+// 
+//
+// Example 3: 
+//
+// 
+//Input: text1 = "abc", text2 = "def"
+//Output: 0
+//Explanation: There is no such common subsequence, so the result is 0.
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= text1.length, text2.length <= 1000 
+// text1 and text2 consist of only lowercase English characters. 
+// 
+// Related Topics String Dynamic Programming 
+// 👍 4932 👎 57
+
+import java.util.logging.Logger;
+public class _1143_LongestCommonSubsequence{
+    private static final Logger logger = Logger.getLogger(_1143_LongestCommonSubsequence .class.toString());
+    public static void main(String[] args) {
+        long startTimeMillis = System.currentTimeMillis();
+        Solution solution = new _1143_LongestCommonSubsequence().new Solution();
+        String text1 = "abcde";
+        String text2 = "ace";
+        solution.longestCommonSubsequence(text1, text2);
+        logger.warning(String.valueOf(solution));
+        logger.info("time cost: [" + (System.currentTimeMillis() - startTimeMillis) + "] ms");
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int n = text1.length();
+        int m = text2.length();
+        int [][] dp = new int[n+1][m+1];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if(text1.charAt(i) == text2.charAt(j)){
+                    dp[i+1][j+1] = dp[i][j]+1;
+                } else {
+                    dp[i+1][j+1] =Math.max(dp[i][j+1], dp[i+1][j]);
+                }
+            }
+        }
+        //System.out.println(dp[n][m]);
+        return dp[n][m];
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ leetcode/editor/en/_300_LongestIncreasingSubsequence.java
@@ -0,0 +1,82 @@
+//Given an integer array nums, return the length of the longest strictly increas
+//ing subsequence. 
+//
+// A subsequence is a sequence that can be derived from an array by deleting som
+//e or no elements without changing the order of the remaining elements. For examp
+//le, [3,6,2,7] is a subsequence of the array [0,3,1,6,2,2,7]. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: nums = [10,9,2,5,3,7,101,18]
+//Output: 4
+//Explanation: The longest increasing subsequence is [2,3,7,101], therefore the 
+//length is 4.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: nums = [0,1,0,3,2,3]
+//Output: 4
+// 
+//
+// Example 3: 
+//
+// 
+//Input: nums = [7,7,7,7,7,7,7]
+//Output: 1
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= nums.length <= 2500 
+// -104 <= nums[i] <= 104 
+// 
+//
+// 
+// Follow up: Can you come up with an algorithm that runs in O(n log(n)) time co
+//mplexity? 
+// Related Topics Array Binary Search Dynamic Programming 
+// 👍 9739 👎 199
+
+import java.util.Arrays;
+import java.util.logging.Logger;
+public class _300_LongestIncreasingSubsequence{
+    private static final Logger logger = Logger.getLogger(_300_LongestIncreasingSubsequence .class.toString());
+    public static void main(String[] args) {
+        long startTimeMillis = System.currentTimeMillis();
+        Solution solution = new _300_LongestIncreasingSubsequence().new Solution();
+        int[] nums = new int[]{10,9,2,5,3,7,101,18};
+        solution.lengthOfLIS(nums);
+        logger.warning(String.valueOf(solution));
+        logger.info("time cost: [" + (System.currentTimeMillis() - startTimeMillis) + "] ms");
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int[] tails = new int[nums.length];
+        int size = 0;
+        for (int x : nums) {
+            int i = 0, j = size;
+            while (i != j) {
+                int m = (i + j) / 2;
+                if (tails[m] < x)
+                    i = m + 1;
+                else
+                    j = m;
+            }
+            tails[i] = x;
+            if (i == size) ++size;
+        }
+        System.out.println(size);
+        return size;
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
